c7d7f543d936902b7bca279f41fd367bc881cf56
be5invis <be5invis@outlook.com>
2014-03-18 05:39:08 +0800

    Optimized package loader

---
+++ bin/youki.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var path = require('path')
+var argv = require('optimist')
+	.boolean('f').describe('f', 'Find and Import .common/')
+	.boolean('debug').describe('debug', 'Enable debug mode')
+	.argv;
+
+var pm = require('../lib/pm.js');
+var scope = pm.init();
+scope['load-library'](scope.directories.loader + '/ove');
+scope.argv = argv._;
+
+if(argv.f) {
+	var cwd = path.resolve(path.dirname(scope.inputPath));
+	do {
+		if(existsSync(path.resolve(cwd, '.common/package.json'))){
+			scope.directories['input-common'] = path.resolve(cwd);
+			scope['load-library'](path.resolve(cwd, '.common/'));
+			break;
+		};
+	} while(cwd !== (cwd = path.resolve(cwd, '..')));
+}
+
+if(argv._[0]) {
+	scope['load-library'](path.resolve(argv._[0]))
+} else {
+	console.error('Missing input')
+}
