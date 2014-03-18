a9fed58cb7751c8c37f3828f55f4f01607a7873e
be5invis <be5invis@outlook.com>
2014-03-19 03:45:09 +0800

    Modified the definition of youki-article macro. Now it supports templates.

--- bin/youki.js
+++ bin/youki.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var path = require('path')
 var argv = require('optimist')
-	.boolean('f').describe('f', 'Find and Import .common/')
+	.boolean('f').describe('f', 'Find and Import .youki-common/')
 	.boolean('debug').describe('debug', 'Enable debug mode')
 	.argv;
 
@@ -13,9 +13,9 @@ scope.argv = argv._;
 if(argv.f) {
 	var cwd = path.resolve(path.dirname(scope.inputPath));
 	do {
-		if(existsSync(path.resolve(cwd, '.common/package.json'))){
+		if(existsSync(path.resolve(cwd, '.youki-common/package.json'))){
 			scope.directories['input-common'] = path.resolve(cwd);
-			scope['load-library'](path.resolve(cwd, '.common/'));
+			scope['load-library'](path.resolve(cwd, '.youki-common/'));
 			break;
 		};
 	} while(cwd !== (cwd = path.resolve(cwd, '..')));
