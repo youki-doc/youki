6f11dec2e62b3337a44923b02d11a3dbe1ca5561
be5invis <belleve@typeof.net>
2017-03-11 20:41:15 +0800

    add npm

--- bin/yki.js
+++ bin/yki.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var path = require('path')
-var argv = require('optimist')
+var argv = require('yargs')
 	.boolean('f').describe('f', 'Find and Import .youki-common/')
 	.boolean('debug').describe('debug', 'Enable debug mode')
 	.argv;
@@ -9,18 +9,18 @@ var pm = require('../lib/pm.js');
 var scope = pm.init();
 scope['load-library'](scope.directories.loader + '/ove');
 
-if(argv._[0]) {
+if (argv._[0]) {
 	var inputPath = path.resolve(argv._[0]);
-	if(argv.f) {
+	if (argv.f) {
 		var cwd = path.dirname(inputPath);
 		do {
-			if(fs.existsSync(path.resolve(cwd, '.youki-common/package.json'))){
+			if (fs.existsSync(path.resolve(cwd, '.youki-common/package.json'))) {
 				scope.directories['input-common'] = path.join(cwd, ".youki-common/");
 				scope.directories['base'] = cwd;
 				scope['load-library'](path.join(cwd, ".youki-common/"));
 				break;
 			};
-		} while(cwd !== (cwd = path.resolve(cwd, '..')));
+		} while (cwd !== (cwd = path.resolve(cwd, '..')));
 	}
 	scope['input-path'] = inputPath
 	scope['load-library'](inputPath)
